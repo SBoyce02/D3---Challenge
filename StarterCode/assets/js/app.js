6cc7120b530506575796511fb285efdfa643c0e5
SBoyce02 <sroecker1983@gmail.com>
2021-05-05 22:30:13 -0500

    moved data csv into app folder

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -13,7 +13,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
+var svg = d3.select(".scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -23,7 +23,7 @@ var chartGroup = svg.append("g")
 
 // Import Data
 d3.csv("data.csv").then(function(CensusData) {
-
+  console.log(CensusData)
     // Step 1: Parse Data/Cast as numbers
     // ==============================
     CensusData.forEach(function(data) {
@@ -48,17 +48,47 @@ d3.csv("data.csv").then(function(CensusData) {
     // Step 3: Create axis functions
     // ==============================
 
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+
     // Step 4: Append Axes to the chart
     // ==============================
+    chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  chartGroup.append("g")
+    .call(leftAxis);
+
+
 
     // Step 5: Create Circles
     // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(hairData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.healthcare))
+    .attr("cy", d => yLinearScale(d.age))
+    .attr("r", "15")
+    .attr("fill", "pink")
+    .attr("opacity", ".5");
 
     // Step 6: Initialize tool tip
     // ==============================
+    var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+      return (`${d.state}<br> Healthcare: ${d.healthcare}<br> Age: ${d.age}`);
+    });
+
 
     // Step 7: Create tooltip in the chart
     // ==============================
+
+    chartGroup.call(toolTip);
 
     // Step 8: Create event listeners to display and hide the tooltip
     // ==============================
@@ -70,12 +100,12 @@ d3.csv("data.csv").then(function(CensusData) {
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Number of Billboard 100 Hits");
+      .text("Healthcare by State");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Hair Metal Band Hair Length (inches)");
+      .text("Age (years)");
   }).catch(function(error) {
     console.log(error);
   });
